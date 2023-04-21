e97f02eee485ffcad9b06b0a99caf13dff57b41f
xPOGx <pavel19.1078@gmail.com>
2023-04-21 18:27:01 +0300

    Implemented Canvas class.

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Canvas {
     private int width;
     private int height;
@@ -7,6 +9,41 @@ public class Canvas {
         this.width = width;
         this.height = height;
         matrix = new char[height][width];
+    }
+
+    public void setPoint(double x, double y, char c) {
+        int a = Math.toIntExact(Math.round(x));
+        int b = Math.toIntExact(Math.round(y));
+
+        if (a >= 0 && a < matrix[0].length && b >= 0 && b < matrix.length) {
+            matrix[b][a] = c;
+        }
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] != 0) {
+                    setPoint(x + j, y + i, c);
+                }
+            }
+        }
+    }
+
+    public void clear() {
+        for (char[] chars : matrix) {
+            Arrays.fill(chars, ' ');
+        }
+    }
+
+    public void print() {
+        for (char[] chars : matrix) {
+            for (char aChar : chars) {
+                System.out.print(aChar + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n");
     }
 
     public int getWidth() {
